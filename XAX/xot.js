acd783ae51c20dd95f9e2182f2f77705c2366d45
Sargis05 <sargis.grigoryan1.g@tumo.org>
2022-02-18 18:51:08 +0400

    verjapes

---
+++ XAX/xot.js
@@ -0,0 +1,63 @@
+
+class Xot {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.directions = [];
+        this.multiply = 5;
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    kill() {
+        var emptyCells2 = this.chooseCell(2);
+        var emptyCells1 = this.chooseCell(1);
+        var newCell2 = emptyCells2[Math.floor(Math.random() * emptyCells2.length)];
+        var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)];
+        if (newCell1 && this.multiply >= 1) {
+            var newX = newCell1[0];
+            var newY = newCell1[1];
+            matrix[newY][newX] = 0;
+            grassEaterArr.splice(newCell1[0], 1);
+            matrix[newCell1[0]][newCell1[1]] = 0;
+            this.multiply = 0;
+        }
+        else if (newCell2 && this.multiply >= 1) {
+            var newX = newCell2[0];
+            var newY = newCell2[1];
+            matrix[newY][newX] = 0;
+            grassPredArr.splice(newCell2[0], 1);
+            matrix[newCell2[0]][newCell2[1]] = 0;
+            this.multiply = 0;
+        }
+        else{
+            this.multiply++;
+        }
+    }
+}
